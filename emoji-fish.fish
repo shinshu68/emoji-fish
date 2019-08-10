22b57993aa2749943f695eca788d02248f7a7b55
shinshu68 <shinshu68@gmail.com>
2019-08-10 21:27:08 +0900

    Add: emoji-fish.fishを追加
    コマンドラインの最後のトークンとマッチするaliasを持つ絵文字をfzfする

---
+++ emoji-fish.fish
@@ -0,0 +1,20 @@
+bind \cs emoji-fish
+
+function emoji-fish
+    set -l buffer (commandline)
+    set -l buffer_len (string length $buffer)
+    set -l token (commandline -t)
+    set -l token_len (string length $token)
+    set -l trim_token_buf (string sub -l (expr $buffer_len - $token_len) $buffer)
+
+    set -l emoji (python suggest.py $token | fzf-tmux -d 25%)
+    if test $status -ne 0
+        false
+    end
+
+    set -l alias (echo $emoji | awk '{print $2}')
+    if test -n '$alias'
+        commandline -P -S
+        commandline -r $trim_token_buf$alias
+    end
+end
